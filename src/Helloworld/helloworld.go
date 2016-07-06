40d37789017c9b3de3d040ca0fbaa25f1fd9805e
liugt <465397036@qq.com>
2016-07-06 17:09:10 +0800

    初次版本建立

---
+++ src/Helloworld/helloworld.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func main() {
+	//按照惯例,如果方法的首字母为大写,则表示这个方法为public,否则为private
+	fmt.Println("这是输出的随机数", rand.Intn(10))
+	// fmt.Printf("hello, world\n")
+	//Reverse("hello, world\n")
+	fmt.Println(swap("Hello", "OGE"))
+}
+func Reverse(s string) string {
+	r := []rune(s)
+	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-i {
+		r[i], r[j] = r[j], r[i]
+	}
+	return string(r)
+}
+
+//go支持一个函数返回多个结果
+func swap(x, y string) (string, string) {
+	return y, x
+}

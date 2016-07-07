e8dbae4355bc3e5f97386789200ec19a2332a89b
liugt <465397036@qq.com>
2016-07-07 08:42:31 +0800

    go中的各个基本类型

---
+++ src/Helloworld/goType.go
@@ -0,0 +1,24 @@
+package Helloworld
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	//bool
+
+	//string
+
+	//int  int8  int16  int32  int64
+	//uint uint8 uint16 uint32 uint64 uintptr
+
+	//byte // uint8的别名
+
+	//rune // int32的别名
+	// represents a Unicode code point
+
+	//float32 float64
+
+	//complex64 complex128
+}
